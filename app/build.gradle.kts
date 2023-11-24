5c4e63eb106bd98b3e86ae145d2a5edecca2345e
Jorge Satorres <jorgesatorres007@gmail.com>
2023-11-24 02:46:53 +0100

    gitflow-feature-stash: MensajesImagenBien

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.proyectoiot"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -49,6 +49,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.google.android.gms:play-services-maps:17.0.1")
+
 
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.firebaseui:firebase-ui-auth:7.1.1")
