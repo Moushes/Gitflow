9e78918a6d94f6b2628c5997e55fd6d5889c6180
Marcos <mmaryus1@epsg.upv.es>
2023-12-22 12:11:37 +0100

    Corre

--- app/src/main/java/com/example/proyectoiot/presentation/bonoPagadoActivity.java
+++ app/src/main/java/com/example/proyectoiot/presentation/bonoPagadoActivity.java
@@ -2,26 +2,51 @@ package com.example.proyectoiot.presentation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.example.comun.Mqtt;
 import com.example.proyectoiot.R;
 
-public class bonoPagadoActivity extends AppCompatActivity {
-
-
+public class bonoPagadoActivity extends Fragment {
     private Mqtt mqtt;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_misbonos);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_misbonos, container, false);
 
         // Inicializa la instancia de Mqtt
         mqtt = new Mqtt();
+        Button entradaButton = view.findViewById(R.id.Entrada);
+        Button salidaButton = view.findViewById(R.id.Salida);
+        Button luzButton = view.findViewById(R.id.Luz);
+        entradaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onEntradaClick();
+            }
+        });
+
+        salidaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSalidaClick();
+            }
+        });
+
+        luzButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLuzClick();
+            }
+        });
+
+        return view;
 
         // Puedes suscribirte a los temas aquí si es necesario
         // mqtt.suscribir("entrada", new MqttCallbackListener());
@@ -30,33 +55,33 @@ public class bonoPagadoActivity extends AppCompatActivity {
     }
 
     // Método para manejar el clic en el botón de Entrada
-    public void onEntradaClick(View view) {
+    public void onEntradaClick() {
         String topic = "entrada";
         String message = "1";
         mqtt.publicar(topic, message);
-        Toast.makeText(this, "Enviando mensaje a " + topic, Toast.LENGTH_SHORT).show();
+        Toast.makeText(requireContext(), "Enviando mensaje a " + topic, Toast.LENGTH_SHORT).show();
     }
 
     // Método para manejar el clic en el botón de Salida
-    public void onSalidaClick(View view) {
+    public void onSalidaClick() {
         String topic = "salida";
         String message = "1";
         mqtt.publicar(topic, message);
-        Toast.makeText(this, "Enviando mensaje a " + topic, Toast.LENGTH_SHORT).show();
+        Toast.makeText(requireContext(), "Enviando mensaje a " + topic, Toast.LENGTH_SHORT).show();
     }
 
     // Método para manejar el clic en el botón de Luz
-    public void onLuzClick(View view) {
+    public void onLuzClick() {
         String topic = "luz";
         String message = "1";
         mqtt.publicar(topic, message);
-        Toast.makeText(this, "Enviando mensaje a " + topic, Toast.LENGTH_SHORT).show();
+            Toast.makeText(requireContext(), "Enviando mensaje a " + topic, Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // Desconecta el cliente MQTT al destruir la actividad
+    public void onDestroyView() {
+        super.onDestroyView();
+        // Desconecta el cliente MQTT al destruir la vista del fragmento
         mqtt.desconectar();
     }
 }
