ea8e99282d658ae4b618039d5a8c2ae1273b7da7
Marcos <mmaryus1@epsg.upv.es>
2024-01-18 19:17:39 +0100

    gitflow-feature-stash: Cambios

--- app/src/main/java/com/example/proyectoiot/presentation/bonoPagadoActivity.java
+++ app/src/main/java/com/example/proyectoiot/presentation/bonoPagadoActivity.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,18 +14,26 @@ import androidx.fragment.app.Fragment;
 
 import com.example.comun.Mqtt;
 import com.example.proyectoiot.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class bonoPagadoActivity extends Fragment {
     private Mqtt mqtt;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_misbonos, container, false);
-
         // Inicializa la instancia de Mqtt
         mqtt = new Mqtt();
         Button entradaButton = view.findViewById(R.id.Entrada);
         Button salidaButton = view.findViewById(R.id.Salida);
         Button luzButton = view.findViewById(R.id.Luz);
+
+        //cambiarfecha();
         entradaButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
